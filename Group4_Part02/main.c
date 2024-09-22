8ba1f8689a4b5e5c083f513c665840c63a3db4af
geethamGT3RS <geetham.gsc@gmail.com>
2024-09-22 23:51:52 +0530

    Reversed order in press in part 2

--- Group4_Part02/main.c
+++ Group4_Part02/main.c
@@ -2,124 +2,110 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-#define STCTRL *((volatile long *)0xE000E010)    // control and status
-#define STRELOAD *((volatile long *)0xE000E014)  // reload value
-#define STCURRENT *((volatile long *)0xE000E018) // current value
+#define STCTRL *((volatile long *)0xE000E010)
+#define STRELOAD *((volatile long *)0xE000E014)
+#define STCURRENT *((volatile long *)0xE000E018)
 
-#define COUNT_FLAG (1 << 16) // bit 16 of CSR automatically set to 1
-#define ENABLE (1 << 0)      // bit 0 of CSR to enable the timer
-#define CLKINT (1 << 2)      // bit 2 of CSR to specify CPU clock
-#define INTERRUPT (1 << 1)   // bit 2 of CSR to specify CPU clock
+#define COUNT_FLAG (1 << 16)
+#define ENABLE (1 << 0)
+#define CLKINT (1 << 2)
 #define CLOCK_MHZ 16
+
 volatile uint32_t timerValue = 0;
-volatile int timerRunning = 0;           // Flag to check if the timer is running
-volatile int f = 100000;                 // Frequency in Hz
-volatile int percent = 5;                // Step size for duty cycle adjustment
-volatile int x = (8 * 1000000) / 100000; // Initial duty cycle
+volatile int timerRunning = 0;
+volatile int f = 100000;
+volatile int percent = 5;
+volatile int x = (8 * 1000000) / 100000;
 volatile int interrupt_count = 0;
 
 void GPIO_F0_INT_HANDLER(void)
 {
-    x = x + (160000 * percent) / f; // Increase x
-    // Ensure x doesn't exceed the maximum value for a valid duty cycle
+    x += (160000 * percent) / f;
     x = ((16000000 / f) <= x) ? (16000000 / f) : x;
 }
 
 void GPIO_F4_INT_HANDLER(void)
 {
-    x = x - (160000 * percent) / f; // Decrease x
-    // Ensure x doesn't go below 1
+    x -= (160000 * percent) / f;
     x = (x <= 0) ? 0 : x;
 }
 
 void GPIO_INIT(void)
 {
-    SYSCTL_RCGC2_R |= 0x00000020;   /* enable clock to GPIOF */
-    GPIO_PORTF_LOCK_R = 0x4C4F434B; /* unlock commit register */
-    GPIO_PORTF_CR_R = 0x1F;         /* make PORTF0 configurable */
-    GPIO_PORTF_DEN_R = 0x1F;        /* set PORTF pins 4 pin */
-    GPIO_PORTF_DIR_R = 0x0E;        /* set PORTF4 pin as input user switch pin */
-    GPIO_PORTF_PUR_R = 0x11;        /* PORTF4 is pulled up */
+    SYSCTL_RCGC2_R |= 0x20;
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R = 0x1F;
+    GPIO_PORTF_DEN_R = 0x1F;
+    GPIO_PORTF_DIR_R = 0x0E;
+    GPIO_PORTF_PUR_R = 0x11;
 }
 
 void Timer0A_Init(void)
 {
-    SYSCTL_RCGCTIMER_R |= 0x01;  // Enable clock for Timer0
-    TIMER0_CTL_R &= ~0x01;       // Disable Timer0A during configuration
-    TIMER0_CFG_R = 0x00;         // Configure for 32-bit timer mode (concatenated)
-    TIMER0_TAMR_R = 0x12;        // TAMR: Up-counting, periodic mode
-    TIMER0_TAILR_R = 0xFFFFFFFF; // Set interval load to max (for long counting)
-    TIMER0_ICR_R = 0x01;         // Clear the timeout flag
-    TIMER0_IMR_R &= ~0x01;       // Disable timer interrupts (not needed for this case)
+    SYSCTL_RCGCTIMER_R |= 0x01;
+    TIMER0_CTL_R &= ~0x01;
+    TIMER0_CFG_R = 0x00;
+    TIMER0_TAMR_R = 0x12;
+    TIMER0_TAILR_R = 0xFFFFFFFF;
+    TIMER0_ICR_R = 0x01;
+    TIMER0_IMR_R &= ~0x01;
 }
 
 void GPIO_INTERRUPT_HANDLER(void)
 {
-    GPIO_PORTF_ICR_R = 0x10; // Clear interrupt flags
-    interrupt_count = interrupt_count + 1;
+    GPIO_PORTF_ICR_R = 0x10;
+    interrupt_count++;
     int temp_x = 5;
-    while (temp_x > 0)
-    {
-        temp_x = temp_x - 1;
-    }
+    while (temp_x-- > 0)
+        ;
     if (timerRunning == 0)
     {
-        // Start Timer
-        TIMER0_CTL_R |= 0x01; // Enable Timer0A
+        TIMER0_CTL_R |= 0x01;
         timerRunning = 1;
     }
     else
     {
-        // Stop Timer
-        TIMER0_CTL_R &= ~0x01;     // Disable Timer0A
-        timerValue = TIMER0_TAR_R; // Read current timer value
-        TIMER0_TAV_R = 0;          // Reset Timer0A counter to 0
+        TIMER0_CTL_R &= ~0x01;
+        timerValue = TIMER0_TAR_R;
+        TIMER0_TAV_R = 0;
         timerRunning = 0;
         if (timerValue < 6500000)
-        {
-            GPIO_F0_INT_HANDLER(); // Call GPIO_F0 handler
-        }
+            GPIO_F0_INT_HANDLER();
         else
-        {
-            GPIO_F4_INT_HANDLER(); // Call GPIO_F4 handler
-        }
+            GPIO_F4_INT_HANDLER();
     }
 }
 
 void Delay(int us)
 {
     STCURRENT = 0;
-    STRELOAD = us;                     // Reload value for 'us' microseconds
-    STCTRL |= (CLKINT | ENABLE);       // Enable the timer
-    while ((STCTRL & COUNT_FLAG) == 0) // Wait until the count flag is set
-    {
+    STRELOAD = us;
+    STCTRL |= (CLKINT | ENABLE);
+    while (!(STCTRL & COUNT_FLAG))
         ;
-    }
-    STCTRL = 0; // Disable timer
-    return;
+    STCTRL = 0;
 }
 
 int main(void)
 {
-    GPIO_INIT(); // Initialize GPIO
+    GPIO_INIT();
     Timer0A_Init();
-    GPIO_PORTF_IS_R = 0x00;  // Edge-sensitive interrupts
-    GPIO_PORTF_IBE_R = 0x10; // Interrupt on single edge
-    GPIO_PORTF_IM_R = 0x10;  // Unmask interrupts for PF4 and PF0
-    NVIC_EN0_R = 0x40000000; // Enable IRQ30 (GPIO Port F interrupt)
+    GPIO_PORTF_IS_R = 0x00;
+    GPIO_PORTF_IBE_R = 0x10;
+    GPIO_PORTF_IM_R = 0x10;
+    NVIC_EN0_R = 0x40000000;
 
     while (1)
     {
-
         if (x != 0)
         {
-            GPIO_PORTF_DATA_R |= 0x02; // Turn on LED connected to PF1
-            Delay(x);                  // Delay depending on the value of x
+            GPIO_PORTF_DATA_R |= 0x02;
+            Delay(x);
         }
         if (x != (16000000 / f))
         {
-            GPIO_PORTF_DATA_R &= ~0x02; // Turn off LED connected to PF1
-            Delay((16000000 / f) - x);  // Delay for the remaining time
+            GPIO_PORTF_DATA_R &= ~0x02;
+            Delay((16000000 / f) - x);
         }
     }
 }
